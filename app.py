4b0a6774ab64f49f9ac4394c7c01b4fe1e41e069
Khysamo <Khysamo@gmail.com>
2025-02-27 10:10:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from PIL import Image 
 
 
 st.title("Hello word")
+
+st.header("En este espacio comienzo a desarrollar mis apps de interfaces multimodales")
+st.write("Facilmente puedo hacer backed y fortend"
+image = Image.open("images.jpeg")
+
+st.image(image, caption="interfaces muildimodales")
 
 
