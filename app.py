20a39710a97b448862c4c4a4689bbeb3d598e8d1
Khysamo <Khysamo@gmail.com>
2025-02-27 10:23:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,14 @@ st.image(image, caption="Ay mi madre el bicho")
 texto = st.text_input("El mejor jugador de la historia"," Un crack goles de cabeza, pierna derecha, pierna izquierda, alto, veloz, fuerte")
 st.write("Esto es el fucking futbol",texto)
 
+st.subheader("Ahora hay dos columnas")
+
+col1,col2 = st.columns(2)
+
+with col1:
+  st.subheader("primera columna")
+  st.write("Todo es mejor si programando estoy")
+  respecto = st.checkbox("Estoy de acuerdo")
+  
+
 
